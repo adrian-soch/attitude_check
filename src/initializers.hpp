6a5c89a9658d277b25ac74f8168d1d659b129100
adrian-soch <adr.soch@outlook.com>
2024-03-18 23:17:13 -0400

    Update quat initializers

--- src/initializers.hpp
+++ src/initializers.hpp
@@ -8,6 +8,13 @@ namespace init {
 template<typename T>
 using Vector3T = Eigen::Matrix<T, 3, 1>;
 
+/**
+ * @brief Computes a quaternion from a single normalized accerleration measurement.
+ *
+ * @tparam T float or double
+ * @param acc Normalized 3x1 accerleration vector
+ * @return quaternion::Quaternion<T>
+ */
 template<typename T>
 quaternion::Quaternion<T> acc_to_quat(const Vector3T<T>& acc)
 {
@@ -16,5 +23,27 @@ quaternion::Quaternion<T> acc_to_quat(const Vector3T<T>& acc)
         std::sqrt(std::pow(acc[1], 2.0) + std::pow(acc[2], 2.0)));
 
     return quaternion::Quaternion(utils::euler_to_quat(roll, pitch, static_cast<T>(0.0)));
+}
+
+/**
+ * @brief Computes a quaternion from normalized accelerometer and magnetometer data.
+ *
+ * @tparam T float or double
+ * @param acc Normalized 3x1 accerleration vector
+ * @param mag Normalized 3x1 magnetometer vector
+ * @return quaternion::Quaternion<T>
+ */
+template<typename T>
+quaternion::Quaternion<T> mag_to_quat(const Vector3T<T>& acc, const Vector3T<T>& mag)
+{
+    T roll  = std::atan2(acc[1], acc[acc[2]]);
+    T pitch = std::atan2(-acc[0],
+        std::sqrt(std::pow(acc[1], 2.0) + std::pow(acc[2], 2.0)));
+
+    T s_pitch = std::sin(pitch), c_pitch = std::cos(pitch);
+    T yaw = std::atan2(mag[2] * s_pitch - mag[1] * c_pitch, mag[0] * std::cos(
+            roll) * (mag[1] * s_pitch + mag[2] * c_pitch));
+
+    return quaternion::Quaternion(utils::euler_to_quat(roll, pitch, yaw));
 }
 } // End namespace init
--- test/initializers_test.cpp
+++ test/initializers_test.cpp
@@ -1,10 +1,13 @@
 #include "gtest/gtest.h"
 #include "initializers.hpp"
 
-TEST(initializers_test_suite, euler_d){
+TEST(initializers_test_suite, acc_d){
     Eigen::Vector3d acc {0.0, 0.0, 9.81};
+    acc.normalize();
 
     quaternion::Quaternion out = init::acc_to_quat(acc);
+
+    std::cout << out.w() << " " << out.x() << std::endl;
     out.normalize();
 
     EXPECT_DOUBLE_EQ(out.w(), 1.0);
@@ -13,8 +16,43 @@ TEST(initializers_test_suite, euler_d){
     EXPECT_DOUBLE_EQ(out.z(), 0.0);
 }
 
-TEST(initializers_test_suite, euler_f){
+TEST(initializers_test_suite, acc_f){
     Eigen::Vector3f acc {0.0f, 0.0f, 9.81f};
+    acc.normalize();
+
+    quaternion::Quaternion out = init::acc_to_quat(acc);
+    out.normalize();
+
+    EXPECT_DOUBLE_EQ(out.w(), 1.0f);
+    EXPECT_DOUBLE_EQ(out.x(), 0.0f);
+    EXPECT_DOUBLE_EQ(out.y(), 0.0f);
+    EXPECT_DOUBLE_EQ(out.z(), 0.0f);
+}
+
+TEST(initializers_test_suite, mag_d){
+    Eigen::Vector3d acc {0.0, 0.0, 9.81};
+    acc.normalize();
+
+    Eigen::Vector3d mag {16676.8, -3050.9, 49916.9};
+    mag.normalize();
+
+    quaternion::Quaternion out = init::mag_to_quat(acc, mag);
+
+    std::cout << out.w() << " " << out.x() << std::endl;
+    out.normalize();
+
+    // EXPECT_DOUBLE_EQ(out.w(), 1.0);
+    // EXPECT_DOUBLE_EQ(out.x(), 0.0);
+    // EXPECT_DOUBLE_EQ(out.y(), 0.0);
+    // EXPECT_DOUBLE_EQ(out.z(), 0.0);
+}
+
+TEST(initializers_test_suite, mag_f){
+    Eigen::Vector3f acc {0.0f, 0.0f, 9.81f};
+    acc.normalize();
+
+    Eigen::Vector3f mag {16676.8f, -3050.9f, 49916.9f};
+    mag.normalize();
 
     quaternion::Quaternion out = init::acc_to_quat(acc);
     out.normalize();
