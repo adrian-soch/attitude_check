499c46f6836641aec4a4b836f9887800f05e5d48
adrian-soch <adr.soch@outlook.com>
2024-03-22 15:47:20 -0400

    Update scipts and configs

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ pip install pre-commit
 pre-commit install
 sudo apt install cmake lcov cppcheck
 sudo apt-get install libgtest-dev && cd /usr/src/gtest && sudo cmake CMakeLists.txt
+sudo apt-get install -y libeigen3-dev && sudo ln -s /usr/include/eigen3/Eigen /usr/local/include/Eigen
 
 echo "Done setup."
