6bf323fdf7ee569deeb3b34fc78d4149a84b049f
adrian-soch <adr.soch@outlook.com>
2024-03-24 17:21:39 -0400

    Fix bug and update unit tests

--- src/attitude_check.cpp
+++ src/attitude_check.cpp
@@ -20,6 +20,8 @@ AttitudeCheck::AttitudeCheck(float imu_gain, float marg_gain, float q0_w, float 
 
 Quat AttitudeCheck::update(Vec3f& acc, Vec3f& gyr, Vec3f& mag, float dt)
 {
+    m_q.normalize();
+
     dt = static_cast<float>(dt);
     if(dt < DT_MIN_SEC) {
         throw std::invalid_argument("Dt is too small.");
@@ -27,61 +29,56 @@ Quat AttitudeCheck::update(Vec3f& acc, Vec3f& gyr, Vec3f& mag, float dt)
 
     if(gyr.norm() <= 0.0f) {
         return m_q;
-    } else if(mag.norm() <= 0.0f) {
+    } else if(mag.norm() <= 0.0f)   {
         return update(acc, gyr, dt);
     }
 
-    m_q.normalize();
     Quat q_dot = (m_q * Quat(0.0f, gyr[0], gyr[1], gyr[2])) * 0.5f;
     if(acc.norm() <= 0.0f) {
-        Quat q_out = m_q + (q_dot * dt);
-        q_out.normalize();
-        return q_out;
+        m_q = m_q + (q_dot * dt);
+        m_q.normalize();
+        return m_q;
     }
 
     acc.normalize();
     mag.normalize();
 
-    Quat h   = m_q * Quat(0.0f, mag[0], mag[1], mag[2]) * m_q.conjugate();
+    Quat h   = m_q * (Quat(0.0f, mag[0], mag[1], mag[2]) * m_q.conjugate());
     float bx = std::sqrt(h.x() * h.x() + h.y() * h.y());
     float bz = h.z();
 
+    /* *INDENT-OFF* */
     Eigen::Matrix<float, 6, 1> func;
     func << 2.0f * (m_q.x() * m_q.z() - m_q.w() * m_q.y()) - acc[0],
-        2.0f * (m_q.w() * m_q.x() - m_q.y() * m_q.z()) - acc[1],
+        2.0f * (m_q.w() * m_q.x() + m_q.y() * m_q.z()) - acc[1],
         2.0f * (0.5f - m_q.x() * m_q.x() - m_q.y() * m_q.y()) - acc[2],
-        2.0f * bx * (0.5f - m_q.y() * m_q.y() - m_q.z() * m_q.z()) + 2.0f * bz
-        * (0.5f - m_q.x() * m_q.z() - m_q.w() * m_q.y()) - mag[0],
-        2.0f * bx * (m_q.x() * m_q.x() - m_q.w() * m_q.z()) + 2.0f * bz
-        * (m_q.w() * m_q.x() + m_q.y() * m_q.z()) - mag[1],
-        2.0f * bx * (m_q.w() * m_q.y() + m_q.x() * m_q.z()) + 2.0f * bz
-        * (0.5f - m_q.x() * m_q.x() - m_q.y() * m_q.y()) - mag[2];
+        2.0f * bx *(0.5f - m_q.y() * m_q.y() - m_q.z() * m_q.z()) + 2.0f * bz * (m_q.x() * m_q.z() - m_q.w() * m_q.y()) - mag[0],
+        2.0f * bx * (m_q.x() * m_q.y() - m_q.w() * m_q.z()) + 2.0f * bz * (m_q.w() * m_q.x() + m_q.y() * m_q.z()) - mag[1],
+        2.0f * bx * (m_q.w() * m_q.y() + m_q.x() * m_q.z()) + 2.0f * bz * (0.5f - m_q.x() * m_q.x() - m_q.y() * m_q.y()) - mag[2];
 
     Eigen::Matrix<float, 4, 6> Jt;
-    Jt << -2.0f * m_q.y(), 2.0f * m_q.x(), 0.0f, -2.0f * bz * m_q.y(), -2.0f * bx * m_q.z() + 2.0f * bz * m_q.x(),
-        2.0f * bx * m_q.y(),
+    Jt << -2.0f * m_q.y(), 2.0f * m_q.x(), 0.0f, -2.0f * bz * m_q.y(), -2.0f * bx * m_q.z() + 2.0f * bz * m_q.x(), 2.0f * bx * m_q.y(),
 
-        2.0f * m_q.z(), 2.0f * m_q.w(), -4.0f * m_q.x(), 2.0f * bz * m_q.z(), 2.0f * bx * m_q.y() + 2.0f * bz * m_q.w(),
-        2.0f * bx * m_q.z() - 4.0f * bz * m_q.x(),
+        2.0f * m_q.z(), 2.0f * m_q.w(), -4.0f * m_q.x(), 2.0f * bz * m_q.z(), 2.0f * bx * m_q.y() + 2.0f * bz * m_q.w(), 2.0f * bx * m_q.z() - 4.0f * bz * m_q.x(),
 
-        -2.0f * m_q.w(), 2.0f * m_q.z(), -4.0f * m_q.y(), -4.0f * bx * m_q.y() - 2.0f * bz * m_q.w(),
-        2.0f * bx * m_q.x() + 2.0f * bz * m_q.z(), 2.0f * bx * m_q.w() - 4.0f * bz * m_q.y(),
+        -2.0f * m_q.w(), 2.0f * m_q.z(), -4.0f * m_q.y(), -4.0f * bx * m_q.y() - 2.0f * bz * m_q.w(), 2.0f * bx * m_q.x() + 2.0f * bz * m_q.z(), 2.0f * bx * m_q.w() - 4.0f * bz * m_q.y(),
 
-        2.0f * m_q.x(), 2.0f * m_q.y(), 0.0f, -4.0f * bx * m_q.z() + 2.0f * bz * m_q.x(),
-        -2.0f * bx * m_q.w() + 2.0f * bz * m_q.y(), 2.0f * bx * m_q.x();
+        2.0f * m_q.x(), 2.0f * m_q.y(), 0.0f, -4.0f * bx * m_q.z() + 2.0f * bz * m_q.x(), -2.0f * bx * m_q.w() + 2.0f * bz * m_q.y(), 2.0f * bx * m_q.x();
+    /* *INDENT-ON* */
 
     Eigen::Matrix<float, 4, 1> grad = Jt * func;
+    grad.normalize();
     q_dot -= (Quat(grad[0], grad[1], grad[2], grad[3]) * m_marg_gain);
 
-    Quat q_next = m_q + (q_dot * dt);
-    q_next.normalize();
+    m_q = m_q + (q_dot * dt);
+    m_q.normalize();
 
-    m_q = q_next;
-    return q_next;
+    return m_q;
 } // AttitudeCheck::update
 
 Quat AttitudeCheck::update(Vec3f& acc, Vec3f& gyr, float dt)
 {
+    m_q.normalize();
     dt = static_cast<float>(dt);
 
     if(gyr.norm() <= 0.0f) {
@@ -90,10 +87,12 @@ Quat AttitudeCheck::update(Vec3f& acc, Vec3f& gyr, float dt)
 
     Quat q_dot = (m_q * Quat(0.0f, gyr[0], gyr[1], gyr[2])) * 0.5;
     if(acc.norm() <= 0.0f) {
-        Quat q_out = m_q + (q_dot * dt);
-        q_out.normalize();
-        return q_out;
+        m_q = m_q + (q_dot * dt);
+        m_q.normalize();
+        return m_q;
     }
+
+    acc.normalize();
 
     Eigen::Matrix<float, 3, 1> func;
     func << 2.0f * (m_q.x() * m_q.z() - m_q.w() * m_q.y()) - acc[0],
@@ -107,12 +106,13 @@ Quat AttitudeCheck::update(Vec3f& acc, Vec3f& gyr, float dt)
         2.0f * m_q.x(), 2.0f * m_q.y(), 0.0;
 
     Eigen::Matrix<float, 4, 1> grad = Jt * func;
+    grad.normalize();
     q_dot -= (Quat(grad[0], grad[1], grad[2], grad[3]) * m_imu_gain);
 
-    Quat q_next = m_q + q_dot * dt;
-    q_next.normalize();
+    m_q = m_q + (q_dot * dt);
+    m_q.normalize();
 
-    return q_next;
+    return m_q;
 } // AttitudeCheck::update
 
 inline void AttitudeCheck::set_quaternion(float q_w, float q_x, float q_y, float q_z)
