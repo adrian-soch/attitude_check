b7b988faf1ff65d8119a14b4b2713e4fcfc2023a
adrian-soch <adr.soch@outlook.com>
2024-03-26 20:43:15 -0400

    Remove eigen dep

--- src/utilities.hpp
+++ src/utilities.hpp
@@ -7,9 +7,14 @@
  */
 #pragma once
 
-#include "quaternion.hpp"
+
+#include <array>
 #include <cmath>
-#include <Eigen/Dense>
+
+#include "quaternion.hpp"
+
+template<typename T>
+using Quat = quaternion::Quaternion<T>;
 
 namespace utils {
 /**
@@ -23,7 +28,7 @@ namespace utils {
  * @return Quaternion
  */
 template<typename T>
-quaternion::Quaternion<T> euler_to_quat(const T& roll, const T& pitch, const T& yaw)
+Quat<T> euler_to_quat(const T& roll, const T& pitch, const T& yaw)
 {
     T c_pitch = cos(pitch * 0.5);
     T c_roll  = cos(roll * 0.5);
@@ -31,7 +36,7 @@ quaternion::Quaternion<T> euler_to_quat(const T& roll, const T& pitch, const T& 
     T s_roll  = sin(roll * 0.5);
 
     if(std::abs(yaw) <= 0.0000001) {
-        return quaternion::Quaternion(c_roll * c_pitch, s_roll * c_pitch, c_roll * s_pitch, -s_roll * s_pitch);
+        return Quat<T>(c_roll * c_pitch, s_roll * c_pitch, c_roll * s_pitch, -s_roll * s_pitch);
     }
 
     T c_yaw = cos(yaw * 0.5);
@@ -40,14 +45,14 @@ quaternion::Quaternion<T> euler_to_quat(const T& roll, const T& pitch, const T& 
     T c_yaw_c_pitch = c_yaw * c_pitch;
     T s_yaw_s_pitch = s_yaw * s_pitch;
 
-    return quaternion::Quaternion(c_yaw_c_pitch * c_roll + s_yaw_s_pitch * s_roll,
+    return Quat<T>(c_yaw_c_pitch * c_roll + s_yaw_s_pitch * s_roll,
              c_yaw_c_pitch * s_roll - s_yaw_s_pitch * c_roll,
              s_yaw * c_pitch * s_roll + c_yaw * s_pitch * c_roll,
              s_yaw * c_pitch * c_roll - c_yaw * s_pitch * c_roll);
 }
 
 template<typename T>
-using Matrix3T = Eigen::Matrix<T, 3, 3>;
+using Matrix3T = std::array<std::array<T, 3>, 3>;
 
 /**
  * @brief Convert Rotation matrix to Quaternion
@@ -57,15 +62,15 @@ using Matrix3T = Eigen::Matrix<T, 3, 3>;
  * @return quaternion::Quaternion<T>
  */
 template<typename T>
-quaternion::Quaternion<T> rotm_to_quat(const Matrix3T<T>& R)
+Quat<T> rotm_to_quat(const Matrix3T<T>& R)
 {
-    T w = std::sqrt(static_cast<T>(1.0) + R(0, 0) + R(1, 1) + R(2, 2)) / 2.0;
+    T w = std::sqrt(static_cast<T>(1.0) + R[0][0] + R[1][1] + R[2][2]) / 2.0;
 
-    const T FOUR_W { w * static_cast<T>(4.0) };
-    T x = (R(2, 1) - R(1, 2)) / FOUR_W;
-    T y = (R(0, 2) - R(2, 0)) / FOUR_W;
-    T z = (R(1, 0) - R(0, 1)) / FOUR_W;
+    const T FOUR_W { w* static_cast<T>(4.0) };
+    T x = (R[2][1] - R[1][2]) / FOUR_W;
+    T y = (R[0][2] - R[2][0]) / FOUR_W;
+    T z = (R[1][0] - R[0][1]) / FOUR_W;
 
-    return quaternion::Quaternion(w, x, y, z);
+    return Quat<T>(w, x, y, z);
 }
 } // End namespace utils
