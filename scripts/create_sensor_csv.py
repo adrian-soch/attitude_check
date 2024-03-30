f0455f2b474392aafdae75fa49847ab7b7bb1070
adrian-soch <adr.soch@outlook.com>
2024-03-30 16:03:08 -0400

    Create accuracy scripts

--- scripts/create_sensor_csv.py
+++ scripts/create_sensor_csv.py
@@ -1,11 +1,23 @@
+'''
+Read data from https://github.com/dlaidig/broad/tree/main/data_hdf5
+And create a .csv file, then multiple methods can operate on that
+data to produce quaternion results.
+
+Results can be compared to each other and the ground truth to determine accuracy,
+and perfromance.
+'''
+
 import scipy.io as sio
 import numpy as np
+import os
 import pandas as pd
 
-file_path = '/home/adrian/Downloads/07_undisturbed_fast_rotation_B.mat'
-# file_path = '/home/adrian/Downloads/01_undisturbed_slow_rotation_A.mat'
+csv_save_path = '.' # Path where file will be saved
+file_path = os.path.expanduser(
+    '~/Downloads/07_undisturbed_fast_rotation_B.mat')
 
 data = sio.loadmat(file_path)
+sampling_rate = (float(data["sampling_rate"]))
 
 gt_quat = np.array(data['opt_quat'])
 gyro = np.array(data['imu_gyr'])
@@ -15,16 +27,14 @@ mag = np.array(data['imu_mag'])
 total = np.hstack([acc, gyro, mag, gt_quat])
 
 df = pd.DataFrame(total)
-df.columns = ['ax', 'ay', 'az', 'gx', 'gy', 'gz','mx', 'my', 'mz','q_gt_w','q_gt_x','q_gt_y','q_gt_z']
-# df = df.dropna(axis = 0, how = 'any')
+df.columns = ['ax', 'ay', 'az', 'gx', 'gy', 'gz', 'mx',
+              'my', 'mz', 'q_gt_w', 'q_gt_x', 'q_gt_y', 'q_gt_z']
 
 i = 13249
-data = df.iloc[i:i+5]
-df.drop(df.columns[[0]], axis=1, inplace=True)
-# df.to_csv('filename.csv', index=False)
+data = df.iloc[i:i+2000]
+print(data)
 
-csv_string = data.to_csv(sep=',', index=False)
-print(csv_string)
+save_path = os.path.join(csv_save_path, 'sample_sensor_data_hz_' + str(sampling_rate) + '.csv')
 
-# latex_table = data.style.to_latex()
-# print(latex_table)
+print(save_path)
+data.to_csv(save_path, index=False)
